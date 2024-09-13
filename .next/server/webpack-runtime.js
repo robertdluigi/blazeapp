ff81667faab4a5137e38587d605db3062e4760ab
Robert <robertdluigi@gmail.com>
2024-09-13 16:51:59 +0300

    Vercel Deploy

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("540f6511a8d8400e")
+/******/ 		__webpack_require__.h = () => ("9d6ffa43b4a47d97")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- lib/riot.ts
+++ lib/riot.ts
@@ -1,6 +1,6 @@
 export const clientID = process.env.RIOT_CLIENT_ID; // Store in .env.local
 export const clientSecret = process.env.RIOT_CLIENT_SECRET; // Store in .env.local
-export const appBaseUrl = process.env.APP_BASE_URL || "https://local.example.com"; // Should be HTTPS
+export const appBaseUrl = process.env.APP_BASE_URL || ""; // Should be HTTPS
 export const appCallbackUrl = `${appBaseUrl}/api/auth/riot/callback`;
 
 export const provider = "https://auth.riotgames.com";
